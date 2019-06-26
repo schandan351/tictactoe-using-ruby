58735bb1f27d3ffb5b554c07d54cc5642188fdb1
chandan sharma <sharmachandan351@gmail.com>
2019-06-26 15:45:34 +0545

    get_next_turn added

--- lib/tictactoe/board.rb
+++ lib/tictactoe/board.rb
@@ -33,24 +33,23 @@ class Board
         print "\n+--------------+"
 
     end
-end
 
 # not_full function
 
-def board_full
-    # check every rows and columns if there is empty position or not
-    for row in 0..BOARD_MAX_INDEX
-        for col in 0..BOARD_MAX_INDEX
-            if @board[row][col]==EMPTY_POS
-                return false
-            end
-        end
+  def board_full
+      # check every rows and columns if there is empty position or not
+      for row in 0..BOARD_MAX_INDEX
+          for col in 0..BOARD_MAX_INDEX
+              if @board[row][col]==EMPTY_POS
+                  return false
+              end
+          end
 
-    end
-    # if board is full
-    return true
+      end
+      # if board is full
+      return true
 
-end
+  end
 
 # winner is checked through
 # In tictactoe winner is determined 
@@ -62,122 +61,133 @@ end
 
 # winner fucntion
 
-def winner
-    winner=winner_row()
-    if winner
-        return winner
-    end
+  def winner
+      winner=winner_row()
+      if winner
+          return winner
+      end
 
-    winner=winner_col()
-    if winner
-        return winner
-    end
-    
-    winner=winner_daigonal()
-    if winner 
-        return winner
-    end
+      winner=winner_col()
+      if winner
+          return winner
+      end
+      
+      winner=winner_daigonal()
+      if winner 
+          return winner
+      end
 
-    # if no winner 
-    return "noone wins"
-end
+      # if no winner 
+      return "noone wins"
+  end
 
 
 # now we have to define winner_row
 
-def winner_row
-    for row_index in 0..BOARD_MAX_INDEX
-        first_symbol=@board[row_index][0]
-        for col_index in 1..BOARD_MAX_INDEX
-            if first_symbol !=@board[row_index][col_index]
-                break    
-            elsif col_index ==BOARD_MAX_INDEX and first_symbol != EMPTY_POS
-                return first_symbol
-            end
-        end
-    end
-    return 
-end
+  def winner_row
+      for row_index in 0..BOARD_MAX_INDEX
+          first_symbol=@board[row_index][0]
+          for col_index in 1..BOARD_MAX_INDEX
+              if first_symbol !=@board[row_index][col_index]
+                  break    
+              elsif col_index ==BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+                  return first_symbol
+              end
+          end
+      end
+      return 
+  end
 
 # same as winner_row we have to define winner_col
 
-def winner_col
-  for col_index in 0..BOARD_MAX_INDEX
-    first_symbol=@board[0][col_index]
+  def winner_col
+    for col_index in 0..BOARD_MAX_INDEX
+      first_symbol=@board[0][col_index]
 
-    for row_index in 1..BOARD_MAX_INDEX
-      if first_symbol !=@board[row_index][col_index]
-        break
-      elsif
-        row_index == BOARD_MAX_INDEX and first_symbol !=EMPTY_POS
-        return first_symbol
+      for row_index in 1..BOARD_MAX_INDEX
+        if first_symbol !=@board[row_index][col_index]
+          break
+        elsif
+          row_index == BOARD_MAX_INDEX and first_symbol !=EMPTY_POS
+          return first_symbol
+        end
       end
     end
+    return 
   end
-  return 
-end
 
 #for diagonnal winner we have to check board[0][0],board[1][1] and [2][2] elements
 # and for reverse diagonal we have to check board[0][max_index_of_board],board[1][max_index_of_board-1],board[2][max_index_of_board-2]
 
-def winner_daigonal
-  first_symbol=@board[0][0]
-    for index in 1..BOARD_MAX_INDEX
-      if first_symbol !=@board[index][index]
-        break
-      elsif index==BOARD_MAX_INDEX and first_symbol!=EMPTY_POS
-        return first_symbol
+  def winner_daigonal
+    first_symbol=@board[0][0]
+      for index in 1..BOARD_MAX_INDEX
+        if first_symbol !=@board[index][index]
+          break
+        elsif index==BOARD_MAX_INDEX and first_symbol!=EMPTY_POS
+          return first_symbol
+        end
       end
-    end
 
-    
-    first_symbol=@board[0][BOARD_MAX_INDEX]
-    row_index=0
-    col_index=BOARD_MAX_INDEX
-
-    while  row_index < BOARD_MAX_INDEX
-      row_index+=1
-      col_index-=1
-
-      if first_symbol !=@board[row_index][col_index]
-        break
-      elsif row_index==BOARD_MAX_INDEX and first_symbol!=EMPTY_POS
-        return first_symbol
-      end
       
-    end
-    return 
-end
+      first_symbol=@board[0][BOARD_MAX_INDEX]
+      row_index=0
+      col_index=BOARD_MAX_INDEX
+
+      while  row_index < BOARD_MAX_INDEX
+        row_index+=1
+        col_index-=1
+
+        if first_symbol !=@board[row_index][col_index]
+          break
+        elsif row_index==BOARD_MAX_INDEX and first_symbol!=EMPTY_POS
+          return first_symbol
+        end
+        
+      end
+      return 
+  end
 
 # ask player for move
 
-def ask_player_for_move(current_player)
-  played=false
-  while not played
-    puts "player" +current_player +"where would you like to play"
-    move=gets.to_i-1
-    col= move % @board.size
-    row=(move-col) % @board.size
+  def ask_player_for_move(current_player)
+    played=false
+    while not played
+      puts "player" +current_player +"where would you like to play"
+      move=gets.to_i-1
+      col= move % @board.size
+      row=(move-col) % @board.size
 
-    if validate_position(row,col)
-      @board[row][col]=current_player
-      played=true
+      if validate_position(row,col)
+        @board[row][col]=current_player
+        played=true
+      end
     end
   end
-end
 
 # now we have to define validate_postion 
 
-def validate_position(row,col)
-  if row<=@board.size and col <=@board.size
-    if @board[row][col]==EMPTY_POS
-      return true
+  def validate_position(row,col)
+    if row<=@board.size and col <=@board.size
+      if @board[row][col]==EMPTY_POS
+        return true
+        else
+          puts "position is occupied"
+        end
       else
-        puts "position is occupied"
+        puts "invalid position"
       end
-    else
-      puts "invalid position"
-    end
-    return false
-end
+      return false
+  end
 
+  # get next turn 
+  def get_next_turn()
+    if @current_player== 'X'
+      @current_player='O'
+    else
+      @current_player='X'
+    end
+    return @current_player
+  end
+
+end
