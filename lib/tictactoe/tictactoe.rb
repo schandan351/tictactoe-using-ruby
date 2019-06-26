d7a0060d3373e25da9c0f3f29455a6a3f5943c22
chandan sharma <sharmachandan351@gmail.com>
2019-06-26 14:28:42 +0545

    loop added for continuoues play and different functions are called that should be defined

---
+++ lib/tictactoe/tictactoe.rb
@@ -0,0 +1,27 @@
+require_relative 'board.rb'
+
+puts ".............starting tictoktoe.........."
+puts
+
+players=['X','O']
+
+# choose  random player
+current_player=players[rand(2)]
+
+b=Board.new(current_player)
+b.display
+
+while b.not_full() and not b.winner()
+    b.ask_player_for_move(current_player)
+    current_player=b.get_next_turn()
+    b.display()
+end
+
+if  b.winner()
+    puts "player" + b.get_next_turn() +"wins"
+else
+    puts "tie game"
+end
+
+puts "GAME OVER"
+
